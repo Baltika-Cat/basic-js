67ab12eb7fdf88cb0587720ea2aacbb35c929c3f
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2023-12-16 00:21:45 +0400

    feat: partly complete simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  count: 0,
+  array: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    return this;
     // remove line with error and write your code here
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
+    this.count += 1;
+    this.array.push(`( ${value} )`);
+    return this;
     // remove line with error and write your code here
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink(position) {
+    this.count += 1;
+    this.array.splice(position - 1, 1);
+    if (!typeof position === 'number' && position % 1 !== 0 && position > this.array.length) {
+      this.array.splice(0, this.array.length);
+      throw new Error(`You can't remove incorrect link!`);
+    }
+    return this;
     // remove line with error and write your code here
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    this.count += 1;
+    this.array.reverse();
+    return this;
     // remove line with error and write your code here
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    this.count += 1;
+    let result = this.array.join(`~~`);
+    this.array.splice(0, this.array.length);
+    return result;
     // remove line with error and write your code here
   }
 };
@@ -30,3 +45,6 @@ const chainMaker = {
 module.exports = {
   chainMaker
 };
+
+console.log(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').removeLink(2).finishChain())
+console.log(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain())
