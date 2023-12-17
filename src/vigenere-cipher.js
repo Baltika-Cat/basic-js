944bd9d513d60b66d1c31bf316e2fb87c39869d3
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2023-12-17 22:22:02 +0400

    feat: complete vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -22,16 +22,16 @@ const { NotImplementedError } = require('../extensions/index.js');
 class VigenereCipheringMachine {
   constructor(bool) {
     this.alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    this.bool = bool;
+    this.bool = bool === false ? bool : true;
   }
   encrypt(string, key) {
-    //try {
+    try {
       let alphabet = this.alphabet;
       if (!string || !key) {
         throw new Error('Incorrect arguments!');
       }
       let len = string.split('').filter((item) => alphabet.includes(item.toLowerCase())).join('').length;
-      console.log(len)
+      //console.log(len)
       let arr = [];
       let arrResult = [];
       while (key.length < len) {
@@ -43,32 +43,38 @@ class VigenereCipheringMachine {
           }
         }
       }
-      console.log(key)
+      //console.log(key)
       let stringLetters = string.split('').filter((item) => alphabet.includes(item.toLowerCase()));
+      //console.log(stringLetters)
       stringLetters.map(function(item, index) {
-        let indexString = alphabet.indexOf(item);
-        let indexKey = alphabet.indexOf(key[index]);
+        let indexString = alphabet.indexOf(item.toLowerCase());
+        let indexKey = alphabet.indexOf(key[index].toLowerCase());
+        //console.log(indexKey)
+        //console.log(indexString)
         let alphaLen = alphabet.length;
-        let indexFinal = indexString + indexKey - 1 > alphaLen ? indexString + indexKey - alphaLen : indexString + indexKey;
+        let indexFinal = indexString + indexKey > alphaLen - 1 ? indexString + indexKey - alphaLen : indexString + indexKey;
+        //console.log(indexFinal)
+        //console.log(alphabet[indexFinal])
         arrResult.push(alphabet[indexFinal].toUpperCase());
       })
       string.split('').map(function(item, index) {
-        if (!alphabet.includes(item)) {
+        if (!alphabet.includes(item.toLowerCase())) {
           arr.push([item, index]);
         }
       })
       arr.forEach((item) => {
         arrResult.splice(item[1], 0, item[0]);
       })
-      console.log(arrResult.join(''), arrResult.reverse().join(''));
-      return !this.bool ? arrResult.reverse().join('') : arrResult.join('');
-    /*} catch(e) {
+      //console.log(arrResult.join(''), arrResult.reverse().join(''));
+      console.log(this.bool)
+      return this.bool ? arrResult.join('') : arrResult.reverse().join('');
+    } catch(e) {
       throw new Error('Incorrect arguments!');
-    }*/
+    }
     // remove line with error and write your code here
   }
   decrypt(string, key) {
-    //try {
+    try {
       let alphabet = this.alphabet;
       if (!string || !key) {
         throw new Error('Incorrect arguments!');
@@ -87,29 +93,30 @@ class VigenereCipheringMachine {
       }
 
       let stringLetters = string.split('').filter((item) => alphabet.includes(item.toLowerCase()));
-      console.log(stringLetters)
+      //console.log(stringLetters)
       stringLetters.map(function(item, index) {
         let indexString = alphabet.indexOf(item.toLowerCase());
-        let indexKey = alphabet.indexOf(key[index]);
+        let indexKey = alphabet.indexOf(key[index].toLowerCase());
         let alphaLen = alphabet.length;
-        let indexFinal = indexString - indexKey + 1 < 0 ? indexString - indexKey + alphaLen : indexString - indexKey;
+        let indexFinal = indexString - indexKey < 0 ? indexString - indexKey + alphaLen : indexString - indexKey;
         arrResult.push(alphabet[indexFinal].toUpperCase());
-        console.log(arrResult)
+        //console.log(arrResult)
       })
       string.split('').map(function(item, index) {
         if (!alphabet.includes(item.toLowerCase())) {
           arr.push([item, index]);
-          console.log(arr)
+          //console.log(arr)
         }
       })
       arr.forEach((item) => {
         arrResult.splice(item[1], 0, item[0]);
       })
-      console.log(arrResult.join(''), arrResult.reverse().join(''));
-      return !this.bool ? arrResult.reverse().join('') : arrResult.join('');
-    /*} catch(e) {
+      //console.log(arrResult.join(''), arrResult.reverse().join(''));
+      //console.log(!this.bool)
+      return this.bool ? arrResult.join('') : arrResult.reverse().join('');
+    } catch(e) {
       throw new Error('Incorrect arguments!');
-    }*/
+    }
     // remove line with error and write your code here
   }
 }
@@ -117,6 +124,17 @@ class VigenereCipheringMachine {
 module.exports = {
   VigenereCipheringMachine
 };
-const directMachine = new VigenereCipheringMachine();
-directMachine.encrypt('attack at dawn!', 'alphonse')
-directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse')
+const directMachine = new VigenereCipheringMachine(false);
+console.log(directMachine.bool)
+//directMachine.encrypt('attack at dawn!', 'alphonse')
+//console.log(directMachine.encrypt('attack at dawn!', 'alphonse'))
+//directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse')
+//directMachine.encrypt('Example of sequence: 1, 2, 3, 4.', 'lilkey')
+let str = 'attack at dawn!';
+str = str.split('').reverse().join('');
+let ke = 'alphonse';
+let encr = directMachine.encrypt(str, ke);
+encr = encr.split('').reverse().join('');
+console.log(encr)
+directMachine.decrypt(encr, ke)
+console.log(directMachine.decrypt(encr, ke))
